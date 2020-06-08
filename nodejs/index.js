bac2f3f113ebfb8d1c8ac0db1ae8bb90d80c643e
punisher077 <arthur.mts@gmail.com>
2020-06-08 15:02:59 -0300

    day 2

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,12 +1,16 @@
+const process = require('process');
+
+process.on("exit", function(){ console.log('Bye');})
+
 function func(value, delay){
         return new Promise((resolve)=> setTimeout(resolve, delay, value));
 }
 
 async function run(){
         console.time('Async');
-        const res1 = await func('hello', 1000);
+        const res1 = await func('hello async', 1000);
         console.log(res1);
-        const res2 = await func('world', 500);
+        const res2 = await func('world async', 500);
         console.log(res2);
         console.timeEnd('Async');
 }
@@ -15,9 +19,10 @@ run();
 
 console.time('Callback');
 
-func('hello', 1000).then(res => console.log(res));
+func('hello cb', 1500).then(res => console.log(res));
 
-func('world', 500).then(res => console.log(res));
+func('world cb', 500).then(res => console.log(res));
 
 console.timeEnd('Callback');
+
 
---
+++ vim/README.md
@@ -0,0 +1,19 @@
+# VIM
+
+Depois de algumas horas quebrando cabeça e escolhendo plugins, cheguei a uma configuração  
+interessante do VIM usando os seguintes plugins:
+
+- COC.NVIM  
+Para autocomplete, documentação e code navigation.  
+- ayu  
+Um tema dark digno de um bom programador.  
+- lightline.vim  
+Uma barra inferior com algumas informações interessantes.
+- editorconfig-vim  
+Integração com arquivos .editorconfig
+- Markdown-preview.nvim  
+Um servidor para mostrar em tempo real alterações em arquivos .MD.
+- NerdTree  
+Uma arvore de arquivos.  
+  
+O arquivo de configuração pode ser encontrado [nesse endereço](https://github.com/arthur-mts/mydotfiles/blob/master/.vimrc).
